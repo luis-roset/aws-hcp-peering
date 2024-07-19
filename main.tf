441c732eba809506d5d3db2f125d125bb7555451
luis-roset <luis.roset@hashicorp.com>
2024-07-19 13:31:41 +0200

    first try

--- main.tf
+++ main.tf
@@ -2,42 +2,60 @@ provider "aws" {
   region = "us-west-2"
 }
 
-provider "tfe" {
+/*provider "tfe" {
   token = var.tfe_token
-}
+}*/
 
 provider "hcp" {
   client_id     = var.hcp_client_id
   client_secret = var.hcp_client_secret
 }
 
+# HCP VPC Creation: Creates an HCP VPC using the hcp_hvn resource
 resource "hcp_hvn" "example" {
+  hvn_id         = "dynamic-hvn"
   project_id = var.hcp_project_id
-  name       = "example-hvn"
+  cloud_provider = "aws"
   region     = "us-west-2"
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "172.25.16.0/20"
 }
-
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+# AWS VPC Creation: Creates an AWS VPC and a subnet.
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 }
 
-resource "aws_vpc_peering_connection" "foo" {
-  peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = aws_vpc.bar.id
-  vpc_id        = aws_vpc.foo.id
+# VPC Peering Connection: Establishes a peering connection between the AWS VPC and the HCP VPC.
+resource "aws_vpc_peering_connection" "example" {
+  vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = hcp_hvn.example.aws_peering_vpc_id
+  peer_owner_id = hcp_hvn.example.aws_account_id
+  peer_region   = "us-west-2"
 
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
+  auto_accept = false
+}
 
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
+# Peering Connection Accepter: Accepts the peering connection on the AWS side.
+resource "aws_vpc_peering_connection_accepter" "example" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.example.id
+  auto_accept               = true
+  vpc_id                    = hcp_hvn.example.aws_peering_vpc_id
+}
+
+# Route Tables Update: Adds routes to the route tables in both VPCs to allow traffic flow.
+
+resource "aws_route" "to_hcp" {
+  route_table_id            = aws_vpc.main.main_route_table_id
+  destination_cidr_block    = hcp_hvn.example.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.example.id
+}
+
+resource "hcp_aws_network_peering_route" "example" {
+  hvn_id      = hcp_hvn.example.id
+  destination = aws_vpc.main.cidr_block
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
-variable tfe_token {
+variable hcp_client_id {
+    description = "TFE token to authenticate to HCP Terraform"
+    type = string
+}
+variable hcp_client_secret {
+    description = "TFE token to authenticate to HCP Terraform"
+    type = string
+}
+variable hcp_project_id {
     description = "TFE token to authenticate to HCP Terraform"
     type = string
 }
